a2326aa1e25aad1f0aa0fe8d22bb885f364363aa
Axel Chaves <axelchaves.r@estudiantec.cr>
2023-06-20 13:39:20 -0600

    predicción a csv realizada

--- conexion.R
+++ conexion.R
@@ -2,42 +2,43 @@ library(forecast)
 library(tidyverse)
 library(ggplot2)
 library(RODBC)
-library(DBI)
 
-setwd("C:/Users/axelc/OneDrive/Escritorio")
+# Paso 1: Establecer una conexión con la base de datos utilizando autenticación de Windows
+con <- odbcConnect("sqlconnect")
+# Reemplaza "nombre_servidor" con el nombre de tu servidor SQL Server
+# Reemplaza "nombre_base_datos" con el nombre de tu base de datos
 
-# Conexión a la base de datos
-connection <- odbcConnect("sqlconnect")
-"""
-# Leer el archivo CSV con punto y coma como delimitador y encabezados
-data <- read.csv('Uber Precios.csv', sep = ";", header = TRUE)
-
-# Guardar los datos en la base de datos
-sqlSave(connection, data, tablename = "Services", rownames = FALSE, append = TRUE)
-"""
 # Paso 2: Ejecutar una consulta SQL para obtener los datos
-consulta <- "SELECT año, mes, ganancia FROM tabla_de_hechos"  # Reemplaza "tabla_de_hechos" con el nombre de tu tabla
-datos <- dbGetQuery(con, consulta)
+consulta <- "SELECT year, month, feePercentage FROM Fact.factTable"  # Reemplaza "tabla_de_hechos" con el nombre de tu tabla
+datos <- sqlQuery(con, consulta)
 
 # Paso 3: Preparar los datos para el análisis
+meses <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
 datos <- datos %>%
-  mutate(fecha = as.Date(paste0(año, "-", mes, "-01"))) %>%
-  select(-mes) %>%
-  group_by(año) %>%
-  summarize(ganancias = sum(ganancia))
+  mutate(month = tolower(month),  # Convertir a minúsculas
+         month = paste0(toupper(substring(month, 1, 1)), substring(month, 2)),  # Primera letra mayúscula
+         month_number = match(month, meses),  # Obtener el número de mes correspondiente
+         fecha = as.Date(paste0(year, "-", sprintf("%02d", month_number), "-01"))) %>%
+  select(-month, -month_number) %>%
+  group_by(year) %>%
+  summarize(ganancias = sum(feePercentage))
 
 # Paso 4: Crear un objeto de serie de tiempo
-serie_tiempo <- ts(datos$ganancias, start = min(datos$año), frequency = 1)
+serie_tiempo <- ts(datos$ganancias, start = min(datos$year), frequency = 2)
 
 # Paso 5: Realizar la predicción
 modelo <- HoltWinters(serie_tiempo)
 prediccion <- forecast(modelo, h = 5)  # Predice 5 años hacia el futuro
 
-# Paso 6: Crear un gráfico de la predicción
-autoplot(modelo) + 
-  autolayer(prediccion, series = "Predicción") +
-  xlab("Año") +
-  ylab("Ganancias") +
-  ggtitle("Predicción de ganancias en años futuros")
-# Cerrar la conexión
-odbcClose(connection)
+# Paso adicional: Convertir la predicción en un dataframe
+prediccion_df <- as.data.frame(prediccion)
+
+# Paso adicional: Agregar una columna con el año
+prediccion_df$year <- seq(max(datos$year) + 1, length.out = nrow(prediccion_df))
+
+# Paso adicional: Guardar la predicción en un archivo CSV
+write.csv(prediccion_df, file = "prediccion.csv", row.names = FALSE)
+
+# Paso 7: Cerrar la conexión con la base de datos
+odbcClose(con)
